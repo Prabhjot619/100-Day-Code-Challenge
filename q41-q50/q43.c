aed65a7cf0f65c937c1ce86b039df38ba75ffe00
Prabhjot Singh <singhprabhjot1510@gmail.com>
2025-10-25 11:03:05 +0530

    Code by Prabhjot singh

--- q41-q50/q43.c
+++ q41-q50/q43.c
@@ -2,35 +2,31 @@
 
 #include <stdio.h>
 
-// Function to calculate factorial of a number
-int factorial(int n) {
-    int fact = 1;
-    for (int i = 1; i <= n; i++) {
-        fact *= i;
-    }
-    return fact;
-}
-
 int main() {
-    int num, temp, digit, sum = 0;
+    int num, temp, digit, fact, sum = 0;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    temp = num;
+    temp=num;
 
-    // Calculate sum of factorials of digits
-    while (temp > 0) {
-        digit = temp % 10;
-        sum += factorial(digit);
-        temp /= 10;
+    while (temp>0) {
+        digit=temp%10;   // get last digit
+        fact=1;
+
+        // find factorial of digit
+        for (int i=1; i<=digit; i++) {
+            fact=fact*i;
+        }
+
+        sum=sum+fact;
+        temp=temp/10;    // remove last digit
     }
 
-    // Check if strong number
-    if (sum == num)
-        printf("%d is a Strong Number.", num);
+    if (sum==num)
+        printf("%d is a Strong number.", num);
     else
-        printf("%d is not a Strong Number.", num);
+        printf("%d is not a Strong number.", num);
 
     return 0;
 }
